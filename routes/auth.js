98259094bcfa113022bdd9556e2bceb174a6ae5a
unnivyshnav <79002656+unnivyshnav@users.noreply.github.com>
2022-02-10 15:56:30 +0530

    Initial commit

---
+++ routes/auth.js
@@ -0,0 +1,59 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { json } = require("express");
+
+//REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPass,
+    });
+
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(400).json("Wrong credentials!");
+
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    !validated && res.status(400).json("Wrong credentials!");
+
+    const { password, ...others } = user._doc;
+    const userid = others._id.valueOf();
+    const useradmin = others.isAdmin;
+    const userpic = others.profilePic;
+    const useremail = others.email;
+    const userusername = others.username;
+
+    console.log(others);
+    // return res.status(200).json(others);
+    // generate acess token
+    others.accessToken = jwt.sign(
+      {
+        id: userid,
+        username: userusername,
+        isAdmin: useradmin,
+        email: useremail,
+      },
+      process.env.SECRET_KEY
+    );
+    return res.status(200).json(others);
+  } catch (err) {
+    // return res.status(500).json(err);
+  }
+});
+
+module.exports = router;
